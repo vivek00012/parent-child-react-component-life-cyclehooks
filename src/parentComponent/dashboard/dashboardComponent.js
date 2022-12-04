7d13da203d343b8ae324f17b24db9c4fa6132ac8
vivek00012 <35500124+vivek00012@users.noreply.github.com>
2022-12-04 10:20:53 +0530

    2 files modified

--- src/parentComponent/dashboard/dashboardComponent.js
+++ src/parentComponent/dashboard/dashboardComponent.js
@@ -2,6 +2,29 @@ import React from 'react';
 
 export default class Dashboard extends React.Component {
   render = () => {
-    return <div>This is Dashboard</div>;
+
+
+    return (<div className="container main-wrapper">
+      <nav>
+        <ul className="nav justify-content-end">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">
+              Latest 
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              React
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Angular
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    )
   };
 }
